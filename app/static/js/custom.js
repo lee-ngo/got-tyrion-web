f30288c63558460016f0dffd0ee8c0fbfe53b212
Lee Ngo <lee.ngo@galvanize.com>
2016-07-06 20:35:32 -0700

    new css, favicon

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,52 +1,52 @@
 $( document ).ready(function(){
 
   $('.next-scene-click').click(function(){
-     $( this ).parent().next().removeClass('hidden');
-     $( this ).parent().hide();
+     $( this ).parent().parent().next().removeClass('hidden');
+     $( this ).parent().parent().hide();
    });
 
 // jquery to go to the top of the wall
   $('#go-top-of-wall').click(function(){
     $('#top-of-wall').removeClass('hidden');
-    $( this ).parent().hide();
+    $( this ).parent().parent().hide();
   })
 
 // jquery to show/hide piss off wall
   $('#go-piss').click(function(){
     $('#piss-off-wall').removeClass('hidden');
-    $( this ).parent().hide();
+    $( this ).parent().parent().hide();
   })
 
 // jquery to show/hide piss no piss off wall
   $('#no-piss').click(function(){
     $('#no-piss-off-wall').removeClass('hidden');
-    $( this ).parent().hide();
+    $( this ).parent().parent().hide();
   })
 
 // jquery to show/hide the random girl in brothel
   $('#choose-random-girl').click(function(){
     $('#random-girl').removeClass('hidden');
-    $( this ).parent().hide();
+    $( this ).parent().parent().hide();
   })
 // jquery to choose tyrion as champion
   $('#go-choose-tyrion').click(function(){
     $('#choose-tyrion').removeClass('hidden');
-    $( this ).parent().hide();
+    $( this ).parent().parent().hide();
   })
 // jquery for choosing jaime
   $('#go-choose-jaime').click(function(){
     $('#choose-jaime').removeClass('hidden');
-    $( this ).parent().hide();
+    $( this ).parent().parent().hide();
   })
 // jquery for choosing bronn
   $('#go-choose-bronn').click(function(){
     $('#choose-bronn').removeClass('hidden');
-    $( this ).parent().hide();
+    $( this ).parent().parent().hide();
   })
 // jquery for accepting hand position
   $('#accept-hand').click(function(){
     $('#hand-king').removeClass('hidden');
-    $( this ).parent().hide();
+    $( this ).parent().parent().hide();
   })
 
 });
